53cfcff0bca350d7aa53d9b6f45a8ed293c18dd2
rotzkirwa <eliud.rotich@student.moringaschool.com>
2024-11-27 12:29:34 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+
+// 1. Define companyName as a constant with the value "Scuber"
+const companyName = "Scuber";
+
+// 2. Declare mostProfitableNeighborhood using let and assign it the value "Chelsea"
+let mostProfitableNeighborhood = "Chelsea";
+
+// 3. Declare companyCeo using let and assign it the value "Susan Smith"
+let companyCeo = "Susan Smith";
